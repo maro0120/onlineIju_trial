eb29469246e9c32ced9f39c9bc109c1e60ed723b
maro0120 <kmanabe0120@gmail.com>
2020-04-26 20:13:09 +0900

    fixed

--- js/intersectionObserver.js
+++ js/intersectionObserver.js
@@ -16,23 +16,3 @@
   });
 })();
 
-
-(() => {
-    const clientHeight = document.documentElement.clientHeight;
-    const header = document.querySelector('.header');
-    let observer = new IntersectionObserver((changes) => {
-        for (const change of changes) {
-            let rect = change.target.getBoundingClientRect();
-            let h = (0 < rect.top && rect.top < clientHeight)       // 対象の上端は表示領域に入っている
-                || (0 < rect.bottom && rect.bottom < clientHeight)  // 対象の下端は表示領域に入っている
-                || (0 > rect.top && rect.bottom > clientHeight);    // 上端下端も表示されてないがその間が表示されている
-            if(h) {
-                    header.classList.remove('scrolled');
-            } else {
-                    header.classList.add('scrolled');
-            }
-        }
-    });
-    let target = document.querySelector('.hero'); // この要素のから外れたら
-    observer.observe(target);
-})();
