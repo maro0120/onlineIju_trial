c845f86e2a58f341fee509f54817fbe6826b999c
maro0120 <kmanabe0120@gmail.com>
2020-05-01 23:00:37 +0900

    fixed

--- js/intersectionObserver.js
+++ js/intersectionObserver.js
@@ -1,3 +1,21 @@
+// (() => {
+//   const sample = document.querySelectorAll('.sample');
+  
+//   const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//       if (entry.intersectionRatio > 0) {
+//         entry.target.classList.add('sample-animation');
+//       } else {
+//         entry.target.classList.remove('sample-animation');
+//       }
+//     });
+//   });
+  
+//   sample.forEach(img => {
+//     observer.observe(img);
+//   });
+// })();
+
 (function () {
   var sample = document.querySelectorAll('.sample');
   var observer = new IntersectionObserver(function (entries) {
@@ -9,7 +27,8 @@
       }
     });
   });
-  sample.forEach(function (img) {
-    observer.observe(img);
+  var nodes = Array.prototype.slice.call(sample, 0);
+  nodes.forEach(function(img){
+    console.log(img);
   });
 })();
